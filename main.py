3acebc077271d6b10264dfe712f197fee69f04ad
Abhivandan Radhakrishnan <abhivandan.off@gmail.com>
2024-08-02 13:42:06 +0530

    modified the main.py to create vector index directly from the documents

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import os
-# from groq import Groq
+from groq import Groq
 from dotenv import load_dotenv
 from llama_index.llms.groq import Groq
+from sentence_transformers import SentenceTransformer
 from llama_index.core import Settings
 from llama_index.core import SimpleDirectoryReader
 from chromadb import PersistentClient
 from llama_index.vector_stores.chroma import ChromaVectorStore
 from llama_index.core import VectorStoreIndex, StorageContext
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-# from llama_index.core.query_engine import TransformQueryEngine
-# from llama_index.core.indices.query.query_transform import HyDEQueryTransform
+from llama_index.core.query_engine import TransformQueryEngine
+from llama_index.core.indices.query.query_transform import HyDEQueryTransform
 # from llama_index.core.query_engine import
 # from llama_ import ch
 
@@ -24,16 +25,20 @@ Settings.embed_model = embed_model
 
 docs = SimpleDirectoryReader("./data").load_data()
 
-db = PersistentClient("./chroma")
-chroma_collection = db.get_or_create_collection("rag_data")
+# db = PersistentClient("./chroma")
+# chroma_collection = db.get_or_create_collection("rag_data")
 
-vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
-storage_context = StorageContext.from_defaults(vector_store=vector_store)
+# vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+# storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
-index = VectorStoreIndex.from_vector_store(
-    vector_store=vector_store,
-    storage_context=storage_context,
-    embed_model = embed_model
+# index = VectorStoreIndex.from_vector_store(
+#     vector_store=vector_store,
+#     storage_context=storage_context,
+#     embed_model = embed_model
+# )
+
+index = VectorStoreIndex.from_documents(
+    documents=docs,
 )
 
 engine = index.as_query_engine(
